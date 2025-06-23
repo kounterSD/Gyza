0b73f2640f2b8a57d0d6510fb373061486a9fba9
panth <panthpatel09@gmail.com>
2025-06-23 02:08:40 +0200

    added gyza_main submodule

--- users/urls.py
+++ users/urls.py
@@ -7,10 +7,10 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
-
+    path('token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
     path('register/', views.UserRegisterView.as_view(), name="register"),
+
     path('profile/', views.UserProfileView.as_view(), name="user_profile")
 ]
 
