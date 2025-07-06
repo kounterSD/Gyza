4456751d785506bd7a7d57b18858c34328b7fe2c
panth <panthpatel09@gmail.com>
2025-07-06 23:01:48 +0200

    Handle auth:expired event. Redirect to /login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,41 @@
 import './css/App.css'
+import {useUser} from "./contexts/UserContext.tsx";
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Explore from './pages/Explore'
 import Landing from './pages/Landing'
 import GlyphDetail from './pages/GlyphDetail'
-import {Route, Routes} from "react-router";
+import {Navigate, Route, Routes} from "react-router";
+import {useNavigate} from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
+    const { logout } = useUser();
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        const handleAuthExpired = () => {
+            logout();
+            navigate('/login');
+        };
+
+        window.addEventListener('auth:expired', handleAuthExpired);
+
+        return () => {
+            window.removeEventListener('auth:expired', handleAuthExpired);
+        };
+    }, [logout, navigate]);
   return (
     <main className="App">
         <Routes>
-            <Route path="/landing" element={<Landing />} />,
-            <Route path="/register" element={<Register />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/explore" element={<Explore />}></Route>
-            <Route path="/glyph/:uuid" element={<GlyphDetail />}></Route>
+            <Route path="/" element={<Navigate to="/landing" replace/>}/>
+            <Route path="/landing" element={<Landing/>}/>
+            <Route path="/register" element={<Register/>}></Route>
+            <Route path="/login" element={<Login/>}></Route>
+            <Route path="/explore" element={<Explore/>}></Route>
+            <Route path="/glyph/:uuid" element={<GlyphDetail/>}></Route>
         </Routes>
+
     </main>
   )
 }
