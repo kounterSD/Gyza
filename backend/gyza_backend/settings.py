6b580dcf5e9afb179cf17fab9614781505808da5
panth <panthpatel09@gmail.com>
2025-07-06 22:47:00 +0200

    CORS config to enable http-only cookies

--- backend/gyza_backend/settings.py
+++ backend/gyza_backend/settings.py
@@ -62,6 +62,20 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'gyza_backend.urls'
 
 TEMPLATES = [
