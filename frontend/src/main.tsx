961ebb2d6ff792d3bb5a7d24e9e4898260f9ec29
panth <panthpatel09@gmail.com>
2025-07-06 23:06:54 +0200

    wrapped in UserProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter } from 'react-router-dom'
 import './css/index.css'
 import App from './App.tsx'
+import {UserProvider} from "./contexts/UserContext.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <UserProvider>
+                <App/>
+            </UserProvider>
+        </BrowserRouter>
+    </StrictMode>,
 )
